bd9460f15a1067647750f0e42ea0dd2f1cda78a2
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-12-11 19:21:50 +0000

    Add student class

--- Typescript/greeter.ts
+++ Typescript/greeter.ts
@@ -1,15 +1,18 @@
+class Student {
+    fullName: string;
+    constructor(public firstName, public middleInitial, public lastName){
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
 interface Person {
     firstName: string;
     lastName: string;
 }
 
 function greeter(person: Person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
+    return "Hello, " + person.firstName + " " + " " + person.lastName;
 }
 
-var person =  {
-    firstName: "Jane",
-    lastName: "User"
-};
+var user =  new Student("Jane", "M.", "User");
 
-document.body.innerHTML = greeter(person);
+document.body.innerHTML = greeter(user);
